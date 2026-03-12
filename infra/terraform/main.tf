0f51cb2f29aa6425b939cd38cc509a1e167f6971
Daniel Rodrigues <58034768+DanielRodrigues7@users.noreply.github.com>
2026-03-12 11:42:13 -0300

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,3 +1,37 @@
+#############################################
+# Resource Group
+#############################################
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}-rg"
+  location = var.location
+}
+
+#############################################
+# App Service Plan (Linux)
+#############################################
+resource "azurerm_service_plan" "plan" {
+  name                = "${var.prefix}-plan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+#############################################
+# Azure Container Registry (ACR)
+#############################################
+resource "azurerm_container_registry" "acr" {
+  name                = replace("${var.prefix}acr", "/[^a-z0-9]/", "")
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
+
+#############################################
+# Web App (Linux) usando Container do ACR
+# COMPATÍVEL COM AZURERM PROVIDER 4.x
+#############################################
 resource "azurerm_linux_web_app" "app" {
   name                = "${var.prefix}-web"
   resource_group_name = azurerm_resource_group.rg.name
@@ -20,4 +54,13 @@ resource "azurerm_linux_web_app" "app" {
   app_settings = {
     WEBSITES_PORT = "80"
   }
+}
+
+#############################################
+# Role Assignment - WebApp pode puxar imagem do ACR
+#############################################
+resource "azurerm_role_assignment" "acr_pull_for_app" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_linux_web_app.app.identity[0].principal_id
 }
