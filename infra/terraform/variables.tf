11271bd914bc1fd09dbbf83814e47768e72387e3
Daniel Rodrigues <58034768+DanielRodrigues7@users.noreply.github.com>
2026-03-12 11:36:42 -0300

    Refactor variable definitions in variables.tf

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,4 +1,19 @@
-variable "prefix"      { type = string  default = "dino" }
-variable "location"    { type = string  default = "brazilsouth" }
-variable "image_name"  { type = string  default = "dino-game" }
-variable "image_tag"   { type = string  default = "bootstrap" }
+variable "prefix" {
+  type    = string
+  default = "dino"
+}
+
+variable "location" {
+  type    = string
+  default = "brazilsouth"
+}
+
+variable "image_name" {
+  type    = string
+  default = "dino-game"
+}
+
+variable "image_tag" {
+  type    = string
+  default = "bootstrap"
+}
