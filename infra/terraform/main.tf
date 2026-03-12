231093f395bb1653e80b850ada462575db1b9fd3
Daniel Rodrigues <58034768+DanielRodrigues7@users.noreply.github.com>
2026-03-12 11:40:01 -0300

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,28 +1,3 @@
-# Resource Group
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.prefix}-rg"
-  location = var.location
-}
-
-# Azure Container Registry
-resource "azurerm_container_registry" "acr" {
-  name                = replace("${var.prefix}acr", "/[^a-z0-9]/", "")
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku                 = "Basic"
-  admin_enabled       = false
-}
-
-# App Service Plan (Linux)
-resource "azurerm_service_plan" "plan" {
-  name                = "${var.prefix}-plan"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  os_type             = "Linux"
-  sku_name            = "B1"
-}
-
-# Web App (Linux) rodando container do ACR
 resource "azurerm_linux_web_app" "app" {
   name                = "${var.prefix}-web"
   resource_group_name = azurerm_resource_group.rg.name
@@ -30,24 +5,19 @@ resource "azurerm_linux_web_app" "app" {
   service_plan_id     = azurerm_service_plan.plan.id
   https_only          = true
 
-  identity { type = "SystemAssigned" }
+  identity {
+    type = "SystemAssigned"
+  }
 
   site_config {
-    application_stack {
-      docker_image     = "${azurerm_container_registry.acr.login_server}/${var.image_name}"
-      docker_image_tag = var.image_tag
+    container_registry_use_managed_identity = true
+
+    container_settings {
+      image_name = "${azurerm_container_registry.acr.login_server}/${var.image_name}:${var.image_tag}"
     }
   }
 
   app_settings = {
-    WEBSITES_PORT     = "80"      # garante bind de porta correto
-    DOCKER_ENABLE_CI  = "true"    # habilita CD de container no App Service
+    WEBSITES_PORT = "80"
   }
-}
-
-# Web App pode puxar imagens privadas do ACR
-resource "azurerm_role_assignment" "acr_pull_for_app" {
-  scope                = azurerm_container_registry.acr.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_linux_web_app.app.identity[0].principal_id
 }
