e4889d84ef3b165bb5e46fdadc805bd5e26d45c0
Daniel Rodrigues <58034768+DanielRodrigues7@users.noreply.github.com>
2026-03-12 11:58:36 -0300

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -29,7 +29,7 @@ resource "azurerm_container_registry" "acr" {
 }
 
 #############################################
-# Web App Linux rodando Docker (provider 4.x)
+# Web App Linux rodando Docker via app_settings
 #############################################
 resource "azurerm_linux_web_app" "app" {
   name                = "${var.prefix}-web"
@@ -43,21 +43,17 @@ resource "azurerm_linux_web_app" "app" {
   }
 
   site_config {
-    application_stack {
-      docker_image     = "${azurerm_container_registry.acr.login_server}/${var.image_name}"
-      docker_image_tag = var.image_tag
-    }
-
     always_on = true
   }
 
   app_settings = {
-    WEBSITES_PORT = "80"
+    DOCKER_CUSTOM_IMAGE_NAME = "${azurerm_container_registry.acr.login_server}/${var.image_name}:${var.image_tag}"
+    WEBSITES_PORT            = "80"
   }
 }
 
 #############################################
-# Permitir o Web App puxar a imagem do ACR
+# Permitir o Web App puxar imagem do ACR
 #############################################
 resource "azurerm_role_assignment" "acr_pull_for_app" {
   scope                = azurerm_container_registry.acr.id
